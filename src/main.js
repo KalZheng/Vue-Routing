99fff7f1cc31f696c79834b534cd2efe21ca7e15
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-09 22:51:06 +0800

    add not found routes, for fall back

--- src/main.js
+++ src/main.js
@@ -5,14 +5,19 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
+        // or use alias but alias will not change url
+        // { path: '/teams', component: TeamsList, alias: '/' },
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
         // props: true allows the :teamId to pass as props instead of $route variable
-        { path: '/teams/:teamId', component: TeamMembers, props: true }, 
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
