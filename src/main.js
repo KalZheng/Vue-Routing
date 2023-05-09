f193b30b989db957806bd8abf44c0d9f0336bd68
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-09 23:24:15 +0800

    multiple router view in one vue, implementation

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
 import NotFound from './components/nav/NotFound.vue'
+import TeamsFooter from './components/teams/TeamsFooter'
+import UsersFooter from './components/users/UsersFooter'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,11 +17,18 @@ const router = createRouter({
         // { path: '/teams', component: TeamsList, alias: '/' },
         {
             //nested routes for display different part to be display
-            name: 'teams', path: '/teams', component: TeamsList, children: [
+            name: 'teams', path: '/teams',
+            // component: TeamsList,
+            components: { default: TeamsList, footer: TeamsFooter },
+            children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },//teams/t1
             ]
         },
-        { path: '/users', component: UsersList },
+        {
+            path: '/users',
+            // component: UsersList
+            components: { default: UsersList, footer: UsersFooter }
+        },
         // props: true allows the :teamId to pass as props instead of $route variable
         // { path: '/teams/:teamId', component: TeamMembers, props: true },
         { path: '/:notFound(.*)', component: NotFound },
